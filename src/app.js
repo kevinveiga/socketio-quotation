8810c380cb226e2b937555a16012128a049232f1
Kevin Veiga <kevin.veiga@gmail.com>
2020-02-05 14:56:27 -0300

    update

---
+++ src/app.js
@@ -0,0 +1,94 @@
+import axios from 'axios';
+import express from 'express';
+import http from 'http';
+import socketIo from 'socket.io';
+
+import { pathPublic, port } from './config.js';
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// CONFIG
+app.use(express.static(pathPublic));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// VARIABLE
+let interval = null;
+
+// FUNCTION
+const getApi = async (socket) => {
+    try {
+        const result = await axios.get('https://api.infomoney.com.br/ativos/ticker?type=json&_=1143');
+
+        socket.emit('quotationData', JSON.stringify(result.data));
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+};
+
+// SOCKET
+// Middleware
+// io.use((socket, next) => {
+//     let token = socket.handshake.query.token;
+
+//     if (isValid(token)) {
+//         return next();
+//     }
+
+//     return next(new Error('authentication error'));
+// });
+
+io.on('connection', (socket) => {
+    console.log('log: new user connected');
+
+    if (interval) {
+        clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+        console.log('interval');
+        getApi(socket);
+    }, 30000);
+
+    socket.on('disconnect', (reason) => {
+        clearInterval(interval);
+
+        console.info('User disconnect: ', reason);
+    });
+
+    socket.on('disconnecting', (reason) => {
+        console.info('User disconnecting: ', reason);
+    });
+
+    socket.on('error', (error) => {
+        console.error('error: ', error);
+    });
+});
+
+// Don't use to event name
+// error
+// connect
+// disconnect
+// disconnecting
+// newListener
+// removeListener
+// ping
+// pong
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
---
+++ src/config.js
@@ -0,0 +1,5 @@
+import path from 'path';
+
+export const pathPublic = path.join(__dirname, '/../public');
+
+export const port = process.env.PORT || 9000;
